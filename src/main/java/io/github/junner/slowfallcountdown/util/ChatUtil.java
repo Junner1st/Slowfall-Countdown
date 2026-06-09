7357369d10e6f3dc93f3fa71c312ccd94275e6fc
Junner1st <jimmy.zm.7219@gmail.com>
2026-06-10 05:57:35 +0800

    fix: use client thread

--- src/main/java/io/github/junner/slowfallcountdown/util/ChatUtil.java
+++ src/main/java/io/github/junner/slowfallcountdown/util/ChatUtil.java
@@ -4,10 +4,14 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class ChatUtil {
-    private static MinecraftClient mc = MinecraftClient.getInstance();
-
     public static void sendMsg(String message) {
-        if (mc.world == null) return;
-        mc.inGameHud.getChatHud().addMessage(Text.literal(message));
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.execute(() -> {
+            if (client.world == null || client.inGameHud == null) {
+                return;
+            }
+
+            client.inGameHud.getChatHud().addMessage(Text.literal(message));
+        });
     }
 }
